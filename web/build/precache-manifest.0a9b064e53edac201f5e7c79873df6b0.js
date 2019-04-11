0c5e9baace1c251160f7ad14939e2aabdb5fe378
Tony161 <mr.sin161@mail.ru>
2019-04-11 13:17:33 +0300

    Procfile

---
+++ web/build/precache-manifest.0a9b064e53edac201f5e7c79873df6b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2967d922f0ced283a08e",
+    "url": "/static/js/main.2967d922.chunk.js"
+  },
+  {
+    "revision": "91dc722beaf7d3fa5c40",
+    "url": "/static/js/1.91dc722b.chunk.js"
+  },
+  {
+    "revision": "2967d922f0ced283a08e",
+    "url": "/static/css/main.51d75eac.chunk.css"
+  },
+  {
+    "revision": "9e1b73ad3db9e4a1d474248ceb6d3c8a",
+    "url": "/index.html"
+  }
+];
